3d71317b8796ef19f1a6d0597f6d113616ba0e90
reynolds119 <reynoldsodame7@gmail.com>
2025-10-22 04:20:52 +0000

    Add files via upload

---
+++ gh resulg/service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("my-gh-result-cache").then((cache) => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
